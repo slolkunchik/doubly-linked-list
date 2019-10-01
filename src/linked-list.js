d599675d87954e6ad0751b2aee1a6f2a4a8608c8
Marina Sevkovich <slolkunchik@gmail.com>
2019-10-01 12:17:44 +0300

    linked-list/feat: add decisions for 10 tests

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,21 +1,89 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
 
-    append(data) {}
+    }
 
-    head() {}
+    append(data) {
+        const newNode = new Node(data);
 
-    tail() {}
+        if(this.length === 0) {
+            this._head = newNode;
+            this._tail = newNode;
+        } else {
+            this._tail.next = newNode;
+            newNode.prev = this._tail;
+            this._tail = newNode;
 
-    at(index) {}
+            if(this.length === 1) {
+                this._head.next = newNode;
+                newNode.prev = this._head;
+            }
+        }
+        this.length += 1;
 
-    insertAt(index, data) {}
+    }
 
-    isEmpty() {}
+    head() {
+        return this._head.data;
+    }
 
-    clear() {}
+    tail() {
+        console.log(this._tail.data);
+        return this._tail.data;
+    }
+
+    at(index) {
+        if (index > -1) {
+            let curNode = this._head;
+            let i = 0;
+            while ((curNode !== null) && (i < index)) {
+                curNode = curNode.next;
+                i++;
+            }
+             if(curNode !== null){
+                 return curNode.data;
+             }
+        }
+    }
+
+    insertAt(index, data) {
+
+        if (index > -1) {
+            const newNode= new Node(data);
+            let curNode = this._head;
+            let i = 0;
+            while ((curNode !== null) && (i < index)) {
+                curNode = curNode.next;
+                i++;
+            }
+            newNode._next = curNode;
+            if(curNode.prev === null) {
+                newNode.prev = null;
+                this._head = newNode;
+            } else {
+                newNode.prev = curNode.prev;
+                curNode.prev.next = newNode;
+            }
+            curNode.prev = newNode;
+            newNode.next = curNode;
+        }
+
+    }
+
+    isEmpty() {
+        return this.length === 0;
+    }
+
+    clear() {
+        this.length = 0;
+        this._head.data = null;
+        this._tail.data = null;
+    }
 
     deleteAt(index) {}
 
